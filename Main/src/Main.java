001f60ba84cfcd9d3280296c89210d4f82daaed6
DESKTOP-S2KG25L\mv <majcieks2@gmail.com>
2017-03-07 22:22:49 +0100

    added button and text input dialog

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -2,15 +2,23 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputControl;
+import javafx.scene.control.TextInputDialog;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
+import javafx.scene.text.Text;
+
 
 import javax.swing.border.Border;
 
+
 public class Main extends Application {
  Stage mainWindow;
-
+ public TextField textFieldNewTODO = new TextField();
  public static void main(String []args)
  {
   launch(args);
@@ -19,22 +27,20 @@ public class Main extends Application {
 
   mainWindow = primaryStage;
 
-  mainWindow.setTitle("TimeSaver");
+  mainWindow.setTitle("Time Saver");
 
 
    BorderPane border = new BorderPane();
    HBox topButtons = addHbox();
    border.setTop(topButtons);
+   border.setCenter(addGridPane());
  //  addStackPane(topButtons);
    Scene scene = new Scene(border, 500, 500);
 
-mainWindow.setScene(scene);
-mainWindow.show();
 
 
-
-
-
+ mainWindow.setScene(scene);
+ mainWindow.show();
 
 
   }
@@ -43,6 +49,9 @@ mainWindow.show();
    final  int prefHeight = 20;
    final int prefWidth = 90;
 
+
+
+   //TOP BAR WITH BUTTONS
 
    HBox topButtons = new HBox();
    topButtons.setPadding(new Insets(1,15,2,15));
@@ -65,9 +74,59 @@ mainWindow.show();
    buttonSettings.setPrefSize(prefWidth, prefHeight);
 
 
-
    topButtons.getChildren().addAll(buttonToDo, buttonEisenhover, buttonStatistics, buttonNotes, buttonSettings);
    return topButtons;
 
   }
+
+  public GridPane addGridPane(){
+   GridPane grid = new GridPane();
+   grid.setHgap(10);
+   grid.setVgap(10);
+   grid.setPadding(new Insets(5, 10 ,5 ,10));
+
+
+   TextInputDialog addNewActivityTODO = new TextInputDialog("Input here");
+   addNewActivityTODO.setTitle("Add new activity");
+   addNewActivityTODO.setHeaderText("DUPA");
+   addNewActivityTODO.setContentText("HERE");
+
+
+
+   Button addNewTODO = new Button("Add sth");
+   addNewTODO.setOnMouseClicked( e -> addNewActivityTODO.show() );
+  grid.add(addNewTODO, 1, 0);
+
+
+   //Category in column 2, row 1
+   Text category = new Text("postponed");
+   category.setFont(Font.font("Arial", FontWeight.BOLD, 22));
+   grid.add(category, 1, 5);
+
+
+   // Title in column 3, row 1
+   Text chartTitle = new Text("sdg sdf sdafgg");
+   chartTitle.setFont(Font.font("Arial", FontWeight.BOLD, 20));
+   grid.add(chartTitle, 2, 10);
+
+   // Subtitle in columns 2-3, row 2
+   Text chartSubtitle = new Text("sth");
+   grid.add(chartSubtitle, 1, 6, 2, 1);
+
+
+//   StackPane root = new StackPane(textFieldNewTODO);
+//   Scene scene2 = new Scene(root , 350, 120);
+//   mainWindow.setScene(scene2);
+//   mainWindow.show();
+
+
+   return grid;
+  }
+
+
+
+
+
+
+
 }
